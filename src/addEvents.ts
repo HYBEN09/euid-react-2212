ab606906d68b58deff406a6ef5b1e1c0f988c174
hyebeen byun <hyebeen2658@naver.com>
2022-11-29 14:29:13 +0900

    learn typescript overview

---
+++ src/addEvents.ts
@@ -0,0 +1,5 @@
+export function addEvents(element: Element, eventMap: { [ key : string ]:() => void}) {
+  for (const [eventType, eventListener] of Object.entries(eventMap)) {
+    element.addEventListener(eventType, eventListener);
+  }
+}
