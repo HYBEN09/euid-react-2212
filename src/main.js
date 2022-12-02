5134a8a95233ff36c2dcd7351769959c0ee35ad7
hyebeen byun <hyebeen2658@naver.com>
2022-12-02 23:01:15 +0900

    Webpack 모듈 번들러 환경 구성

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { getRandomMinMax, styleLog } from "./utils/index";
+
+function handleBodyClick() {
+  styleLog("clicked body", "color: #2124ff");
+
+  let randomNumber = getRandomMinMax(10, 89);
+  styleLog(`난수: ${randomNumber}`, "color: #4caf51");
+}
+
+document.addEventListener("click", handleBodyClick);
